94e31868e5f9e7085dd2c624a6e80bdc44066bf0
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-09-16 15:21:02 +0300

    add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Saira } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const saira = Saira({
+  subsets: ["latin"], 
+  weight: ["400", "500", "600", "700"], 
+  variable: "--font-saira", 
 });
 
 export const metadata: Metadata = {
@@ -25,9 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+     className={saira.variable}
       >
-        {children}
+        <Header />
+        <main>{children}</main>
       </body>
     </html>
   );
